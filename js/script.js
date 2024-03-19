9f413c66cf1ef4799b7958eeb991a66b3a46a50a
dmasandeepa <anushka@amerck.com>
2024-03-19 21:39:45 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,102 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Engineer', 'Graphic Designer'],
+    typeSpeed: 100,
+    backSpeed: 40,
+    backDelay: 1000,
+    loop: true
+});
+
+/*==================== read more button ====================*/
+
+function toggleDescription(descriptionId, btnId) {
+    var description = document.getElementById(descriptionId);
+    var btn = document.getElementById(btnId);
+
+    if (description.classList.contains("collapsed")) {
+        // Expand the description
+        description.classList.remove("collapsed");
+        description.style.height = "auto";
+        btn.textContent = "See Less";
+    } else {
+        // Collapse the description
+        description.classList.add("collapsed");
+        description.style.height = "10vh"; // Change to the initial height of the collapsed description
+        btn.textContent = "Read More";
+    }
+}
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
+
+
+
